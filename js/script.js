fe2c46492627495ff716dc9abcedc4b11f4c9e29
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-09-13 16:17:30 -0300

    atualização de pequenos bug e novas musicas

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ const audio = document.querySelector('audio');
 
 const songList = [
     {path:'song/Naruto Shippuden.mp3', songName:'Naruto Remix'},
+    {path:'song/Ikimono Gakari  Blue Bird Traduo.mp3', songName:'Blue Bird'},
+    {path:'song/Ikimono Gakari Traduo Portugus Blue Bird PTBR.mp3', songName:'Blue Bird PTBR'},
+    {path:'song/Rap do Naruto  Tauz RapTributo 05.mp3', songName:'Tauz Naruto I'},
+    {path:'song/Rap do Naruto Remake  Tauz RapTributo 73.mp3', songName:'Tauz Naruto II'},
+    {path:'song/Rap do Rock Lee Naruto  Tauz 44.mp3', songName:'Tauz Rock Lee'},
+    {path:'song/Rap do Gaara Naruto.mp3', songName:'Tauz Gaara'},
+    {path:'song/Rap do Minato Naruto  Tauz RapTributo 01.mp3', songName:'Tauz Minato'},
+    {path:'song/Rap do Jiraiya Naruto  Tauz RapTributo 48.mp3', songName:'Tauz Jiraiya'},
+    {path:'song/Rap do Kakashi Naruto  Tauz RapTributo 09.mp3', songName:'Tauz Kakashi'},
+    {path:'song/Rap do Obito Naruto  Tauz RapTributo 38.mp3', songName:'Tauz Obito'},
+    {path:'song/Rap do Madara Naruto l Tributo 11.mp3', songName:'Tauz Madara'},
+    {path:'song/Rap do Shisui Naruto  Tauz RapTributo 54.mp3', songName:'Tauz Shisui'},
+    {path:'song/Rap do Itachi Naruto  Tauz RapTributo 18.mp3', songName:'Tauz Itachi'},
+    {path:'song/Rap do Sasuke Pt II Naruto  Tauz RapTributo 19.mp3', songName:'Tauz Sasuke'},
 ];
 
 let song_Playing = false;
